7d338aed0c88b9ce5cd7529357e4724ee00d86bc
Daniel Tsivkovski <dtsivkovs@gmail.com>
2024-04-08 03:11:20 -0700

    image shutter

--- assets/imagebox.js
+++ assets/imagebox.js
@@ -4,23 +4,33 @@
 var imagebox = document.querySelector("#imagebox");
 var imageChild = document.querySelector("#imagebox img");
 // add event listener to the imagebox
-imageChild.addEventListener("click", ib_onclick);
+imagebox.addEventListener("click", ib_onclick);
 
-const imageArray = ["assets/images/main_headshot.jpg", "assets/images/meme1.jpg"];
+const imageArray = ["assets/images/main_headshot.jpeg", "assets/images/luna1.jpg", "assets/images/camille1.jpg", "assets/images/panda1.jpg", "assets/images/luna2.jpg", "assets/images/camille2.jpg", "assets/images/panda2.jpg", "assets/images/luna3.jpg", "assets/images/camille3.jpeg", "assets/images/panda3.jpg", "assets/images/camille-panda-together.jpg"];
 
-var image_state = 0;
+var image_state = 1;
+var isExpanded = false;
+
+function imageState() {
+    if (image_state > imageArray.length - 1) {
+        image_state = 0;
+    }
+    imagebox.classList.add("imagebox-shutter");
+    setTimeout(function() {
+        imageChild.src = imageArray[image_state++];
+    }, 200);
+    setTimeout(function() {
+        imagebox.classList.remove("imagebox-shutter");
+    }, 601);
+}
 
 // function to expand or shrink the imagebox
 function ib_onclick() {
-    // check if the imagebox is expanded
-    switch (image_state) {
-        case 0:
-            imagebox.classList.add("imagebox-expanded");
-            image_state = 1;
-            break;
-        case 1:
-            imagebox.classList.remove("imagebox-expanded");
-            image_state = 0;
-            break;
+    if (!isExpanded) {
+        imagebox.classList.add("imagebox-expanded");
+        isExpanded = true;
+    }
+    else {
+        imageState();
     }
 }
